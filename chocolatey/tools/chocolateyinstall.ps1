d5cd0e563809ff9d7131c268c8562a496052cd87
logan <sonjm0428@gmail.com>
2025-09-30 15:07:40 +1000

    Update package files for release v0.1.0

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   url64bit      = $url64
   softwareName  = 'dororong*'
-  checksum64    = 'CHECKSUM64_PLACEHOLDER'
+  checksum64    = '0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5'
   checksumType64= 'sha256'
 }
 
