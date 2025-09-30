e6b29a8f3c9e5593a123069441822f0cf4e23713
logan <sonjm0428@gmail.com>
2025-09-30 15:58:43 +1000

    remove packaging files

--- chocolatey/tools/chocolateyuninstall.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-$packageName = 'dororong'
-
-# Remove the shim
-Uninstall-ChocolateyShim -Name 'dororong'
-
-Write-Host "dororong has been uninstalled successfully." -ForegroundColor Green
