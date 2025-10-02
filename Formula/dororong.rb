fe76ce48d94bec3b41639c3aabe80b3e7cd4651a
logan <sonjm0428@gmail.com>
2025-10-02 13:48:09 +1000

    chore: Update formula SHA256 for v0.1.1

--- Formula/dororong.rb
+++ Formula/dororong.rb
@@ -2,7 +2,7 @@ class Dororong < Formula
   desc "Fun terminal animation app with dancing characters"
   homepage "https://github.com/AbletonPilot/dororong"
   url "https://github.com/AbletonPilot/dororong/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "b2272a71d124dae5373591535b94a449c0f643e322b339eaba113ea48b6f88c8"
   license "MIT"
   head "https://github.com/AbletonPilot/dororong.git", branch: "main"
 
