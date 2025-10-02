b49f7eb21358ca79b8d14925064ae15524d81d9b
logan <sonjm0428@gmail.com>
2025-10-02 13:46:49 +1000

    chore: Bump version to 0.1.1
    - Update Cargo.toml version to 0.1.1
    - Update Cargo.lock
    - Update Homebrew formula for v0.1.1
    - Improve prepare_release.sh script

--- Formula/dororong.rb
+++ Formula/dororong.rb
@@ -1,8 +1,8 @@
 class Dororong < Formula
   desc "Fun terminal animation app with dancing characters"
   homepage "https://github.com/AbletonPilot/dororong"
-  url "https://github.com/AbletonPilot/dororong/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "4fca3377d862165fdef8d700f0582528cb92f4619ad9ff2b45cf39add61b0a94"
+  url "https://github.com/AbletonPilot/dororong/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "PLACEHOLDER_SHA256"
   license "MIT"
   head "https://github.com/AbletonPilot/dororong.git", branch: "main"
 
@@ -19,7 +19,7 @@ class Dororong < Formula
 
   test do
     assert_match "dororong", shell_output("#{bin}/dororong --help")
-    assert_match "0.1.0", shell_output("#{bin}/dororong --version")
+    assert_match "0.1.1", shell_output("#{bin}/dororong --version")
   end
 end
 
